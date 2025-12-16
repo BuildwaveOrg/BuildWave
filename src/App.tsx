21fcdb80921669635b62c30b4eeae53d6d80d2d6
Tigo-cmd <ja16162040@gmail.com>
2025-12-16 14:21:36 +0100

    feat: updated change to App.tsx by adding the basename="/BuildWave/" to the BrowserRouter. This fix needs to be deployed for your users to see the corrected routing behavior.

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/BuildWave/">
           <WhatsappChat />
           <Routes>
             <Route path="/" element={<Index />} />
