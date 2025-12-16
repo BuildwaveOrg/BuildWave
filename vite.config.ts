9daf0ee2799e011ed2c09d2a2eef72ea8850705c
Tigo-cmd <ja16162040@gmail.com>
2025-12-16 14:25:37 +0100

    updatd

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,12 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/BuildWave/",
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: "/BuildWave/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
